0698e7240ad04450790dcec19a59b1c3ce1f4109
Samir <samirstown@hotmail.com>
2025-11-09 20:44:51 -0800

    Continued adding to components

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from 'react-router';
+
+
+
 function ProductList(){
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
     return (
         <>
         <h1 className="mt-5">Product List</h1>
+
         </>
         
-    )
+    ) 
 }
 
 export default ProductList;

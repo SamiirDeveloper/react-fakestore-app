de0bb5c27919e0abe0bc5179fabb5e2263253093
Samir <samirstown@hotmail.com>
2025-12-08 10:32:14 -0800

    fixing cart issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/'
 import './index.css'
 import App from './App.jsx'
+import { CartProvider } from './components/Cart.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
